d7aed4bec03d0ccac6a0c340f65f0d4b0cbc340d
shujaatsiddiqui <shujaat.siddiqui@speridian.com>
2024-11-07 22:19:28 -0800

    Initial commit

---
+++ sampleremotemfeapi/Controllers/TodoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace sampleremotemfeapi.Controllers
+{
+    [Authorize] // Protects the entire controller or specific actions
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        [HttpGet()]
+        public ActionResult<List<string>> GetTodoList()
+        {
+            List<string> todoList = new List<string>
+            {
+                "Finish project report",
+                "Update client on progress",
+                "Review pull requests",
+                "Schedule team meeting",
+                "Prepare presentation slides"
+            };
+
+            return todoList;
+        }
+    }
+}
